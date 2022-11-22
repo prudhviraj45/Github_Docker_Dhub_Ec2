5d030c403ccc6a5f2a364be18b8063ee68e3ede8
prudhviraj45 <115442733+prudhviraj45@users.noreply.github.com>
2022-11-22 17:05:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,23 @@
-on:
-  push:
-    branches: [ master ]
-  pull_request:
-    branches: [ master ]
-name: AWS GITHUB-DCOKERHUB-EC2 Deployment
-jobs:
-  deploy:
-    name: Deploy
-    runs-on: ubuntu-latest
-    steps:
-    - name: Checkout
-      uses: actions/checkout@v2
-    - name: Login to Docker Hub
-      uses: docker/login-action@v2
-      with:
-        username: ${{ secrets.DOCKERHUB_USERNAME }}
-        password: ${{ secrets.DOCKERHUB_TOKEN }}
-    - name: Build, tag, and push the image to Docker Hub
-      id: build-image
-      env:
-        DOCKERHUB_USERNAME: ${{ secrets.DOCKERHUB_USERNAME }}
-        DOCKER_REPOSITORY: "myrepo"
-        IMAGE_TAG: "v2"
-      run: |
-        # Build a docker container and push it to DOCKERHUB
-        docker build -t $DOCKERHUB_USERNAME/$DOCKER_REPOSITORY:$IMAGE_TAG .
-        echo "Pushing image to DOCKERHUB..."
-        docker push $DOCKERHUB_USERNAME/$DOCKER_REPOSITORY:$IMAGE_TAG
-    - name: executing remote ssh commands using ssh key
-      uses: appleboy/ssh-action@master
-      with:
-        host: ${{ secrets.HOST }}
-        username: ${{ secrets.USERNAME }}
-        key: ${{ secrets.KEY }}
-        port: ${{ secrets.PORT }}
-        script: |
-          docker pull mohanmul789/demo-web-app-ec2:latest
-          #Stop container
-          docker stop demoapp
-          #Remove all untagged images
-          docker system  prune -a -f
-          docker run -d -p 80:80 --name demoapp -it mohanmul789/demo-web-app-ec2
+rom flask import Flask, jsonify
+import time
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"Time of Call Home Page change B": time.time()})
+
+@app.route("/one")
+def one():
+    return jsonify({"Time of Call One": time.ctime()})
+
+@app.route("/two")
+def two():
+    return jsonify({"Time of Call Two": time.time()})
+
+@app.route("/three")
+def three():
+    return jsonify({"Time of Call Three": time.time()})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
